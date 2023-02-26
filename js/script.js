ed0fb2e11886a11059955c0c3884932d263171ca
Raul <42661316+avfreq@users.noreply.github.com>
2023-02-25 18:46:16 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,31 @@
+'use strict!'
+
+function checkTime(i) {
+    if (i < 10) {
+      i = "0" + i;
+    }
+    return i;
+  }
+  
+  function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    // add a zero in front of numbers<10
+    m = checkTime(m);
+    s = checkTime(s);
+    document.querySelector('#time').textContent = h + ":" + m + ":" + s;
+    t = setTimeout(function() {
+      startTime()
+    }, 500);
+  }
+  startTime();
+
+
+
+
+
+
+
+// document.querySelector('#time').textContent = n;
